062b71c4a0a7733704d592dcd17d7cfff49b57ce
Henry Penton <henry.penton@gmail.com>
2025-10-14 16:19:16 +0100

    stack blocks on one another

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -5,7 +5,7 @@ import TflBestRoute from "./TflBestRoute";
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen p-8 bg-[#181818] border-8 border-cyan-300 rounded-none shadow-[0_0_0_8px_#222,0_0_32px_0_#00ffe7] font-mono text-[#f8f8f2]">
+  <main className="w-full min-h-screen p-8 bg-[#181818] font-mono text-[#f8f8f2]">
       <h1
         className="text-center text-cyan-300 text-4xl font-bold tracking-widest mb-10 drop-shadow-[0_0_2px_white,0_0_8px_#00ffe7] font-mono border-b-4 border-yellow-200 pb-4"
         style={{ letterSpacing: "0.15em" }}
@@ -22,14 +22,16 @@ export default function Home() {
         />
 
         <TflLineStatus />
-        <TflBestRoute
-          to={{ placeName: "Paddington", naptan: "940GZZLUPAC" }}
-          from={{ placeName: "Tooting", naptan: "940GZZLUTBY" }}
-        />
-        <TflBestRoute
-          from={{ placeName: "Paddington", naptan: "940GZZLUPAC" }}
-          to={{ placeName: "Tooting", naptan: "940GZZLUTBY" }}
-        />
+        <div className="flex flex-col gap-4 w-full max-w-md">
+          <TflBestRoute
+            to={{ placeName: "Paddington", naptan: "940GZZLUPAC" }}
+            from={{ placeName: "Tooting", naptan: "940GZZLUTBY" }}
+          />
+          <TflBestRoute
+            from={{ placeName: "Paddington", naptan: "940GZZLUPAC" }}
+            to={{ placeName: "Tooting", naptan: "940GZZLUTBY" }}
+          />
+        </div>
       </div>
     </main>
   );
