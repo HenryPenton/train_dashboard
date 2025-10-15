568de7d9b78cd7080937957f47ba04db379a6729
Henry Penton <henry.penton@gmail.com>
2025-10-15 17:51:28 +0100

    standalone with static folder too

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,8 +1,11 @@
 // tailwind.config.js
 export default {
   content: [
-    './index.html',
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
+    './**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
