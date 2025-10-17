9ea60047108183c4eef916eadd73a24a5179a0a6
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2025-10-17 02:43:38 +0100

    split into multiple tests

--- api/src/test/utils/test_tfl_utils.py
+++ api/src/test/utils/test_tfl_utils.py
@@ -1,47 +1,55 @@
 from src.utils.tfl_utils import simplify_tfl_line_status
 
 
-def test_simplify_tfl_line_status_basic():
-    response_json = [
-        {
-            "name": "Victoria",
-            "lineStatuses": [{"statusSeverityDescription": "Good Service"}],
-        },
-        {
-            "name": "Central",
-            "lineStatuses": [{"statusSeverityDescription": "Minor Delays"}],
-        },
-        {
-            "name": "Northern",
-            "lineStatuses": [
-                {"statusSeverityDescription": "Minor Delays"},
-                {"statusSeverityDescription": "Part Suspended"}
-            ],
-        },
-        {
-            "name": "Mildmay",
-            "lineStatuses": [
-                {"statusSeverityDescription": "Part Closure"},
-                {"statusSeverityDescription": "Part Closure"},
-                {"statusSeverityDescription": "Good Service"}
-            ],
-        },
-        {"name": "Jubilee", "lineStatuses": []},
-    ]
-    result = simplify_tfl_line_status(response_json)
-    assert result == [
-        {"name": "Victoria", "status": "Good Service"},
-        {"name": "Central", "status": "Minor Delays"},
-        {"name": "Northern", "status": "Minor Delays, Part Suspended"},
-        {"name": "Mildmay", "status": "Part Closure x2, Good Service"},
-    ]
+class TestSimplifyTflLineStatus:
 
+    def test_one_status(self):
+        response_json = [
+            {
+                "name": "Victoria",
+                "lineStatuses": [{"statusSeverityDescription": "Good Service"}],
+            },
+        ]
+        result = simplify_tfl_line_status(response_json)
+        assert result == [
+            {"name": "Victoria", "status": "Good Service"},
+        ]
 
-def test_simplify_tfl_line_status_empty():
-    assert simplify_tfl_line_status([]) == []
+    def test_two_statuses(self):
+        response_json = [
+            {
+                "name": "Northern",
+                "lineStatuses": [
+                    {"statusSeverityDescription": "Minor Delays"},
+                    {"statusSeverityDescription": "Part Suspended"}
+                ],
+            },
+        ]
+        result = simplify_tfl_line_status(response_json)
+        assert result == [
+            {"name": "Northern", "status": "Minor Delays, Part Suspended"},
+        ]
 
+    def test_two_same_statuses(self):
+        response_json = [
+            {
+                "name": "Mildmay",
+                "lineStatuses": [
+                    {"statusSeverityDescription": "Part Closure"},
+                    {"statusSeverityDescription": "Part Closure"},
+                    {"statusSeverityDescription": "Good Service"}
+                ],
+            },
+        ]
+        result = simplify_tfl_line_status(response_json)
+        assert result == [
+            {"name": "Mildmay", "status": "Part Closure x2, Good Service"},
+        ]
 
-def test_simplify_tfl_line_status_missing_fields_omitted():
-    response_json = [{}, {"name": "Piccadilly"}]
-    result = simplify_tfl_line_status(response_json)
-    assert result == []
+    def test_empty(self):
+        assert simplify_tfl_line_status([]) == []
+
+    def test_missing_fields_omitted(self):
+        response_json = [{}, {"name": "Piccadilly"}]
+        result = simplify_tfl_line_status(response_json)
+        assert result == []
