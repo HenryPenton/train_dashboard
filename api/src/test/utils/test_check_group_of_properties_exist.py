ce58aa43663da48c8c1e6f9b39049ee49d9ac774
Henry Penton <henry.penton@gmail.com>
2025-10-17 21:07:21 +0100

    lint file

--- api/src/test/utils/test_check_group_of_properties_exist.py
+++ api/src/test/utils/test_check_group_of_properties_exist.py
@@ -1,12 +1,13 @@
 import unittest
 from src.utils.check_group_of_properties_exist import check_group_of_properties_exist
 
+
 class TestCheckGroupOfPropertiesExist(unittest.TestCase):
     def test_all_exist(self):
-        self.assertTrue(check_group_of_properties_exist(1, 'a', 0, False, []))
+        self.assertTrue(check_group_of_properties_exist(1, "a", 0, False, []))
 
     def test_one_none(self):
-        self.assertFalse(check_group_of_properties_exist(1, None, 'b'))
+        self.assertFalse(check_group_of_properties_exist(1, None, "b"))
 
     def test_all_none(self):
         self.assertFalse(check_group_of_properties_exist(None, None))
@@ -15,5 +16,5 @@ class TestCheckGroupOfPropertiesExist(unittest.TestCase):
         self.assertTrue(check_group_of_properties_exist())  # No args, so none are None
 
     def test_mixed_types(self):
-        self.assertTrue(check_group_of_properties_exist(0, '', [], {}, False))
-        self.assertFalse(check_group_of_properties_exist(0, '', None, {}, False))
+        self.assertTrue(check_group_of_properties_exist(0, "", [], {}, False))
+        self.assertFalse(check_group_of_properties_exist(0, "", None, {}, False))
