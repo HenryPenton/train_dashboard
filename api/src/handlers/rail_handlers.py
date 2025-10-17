98c1ed87aa2417ff7f74c6792e1d42a16d4d93ee
Henry Penton <henry.penton@gmail.com>
2025-10-17 11:06:15 +0100

    tidy

--- api/src/handlers/rail_handlers.py
+++ api/src/handlers/rail_handlers.py
@@ -3,6 +3,8 @@ from fastapi import HTTPException
 from src.utils.rail_utils import process_departures_response
 from src.clients.rttclient import RTTClient
 
+rtt_client: RTTClient = RTTClient(httpx.AsyncClient())
+
 
 async def get_departures_handler(
     origin_station_code: str, destination_station_code: str
@@ -11,15 +13,13 @@ async def get_departures_handler(
     Get departures from a station using the Real Time Trains API, filtered by destination station code.
     """
 
-    async with httpx.AsyncClient() as client:
-        rtt_client = RTTClient(client)
-        try:
-            data = await rtt_client.get_departures(
-                origin_station_code, destination_station_code
-            )
-            processed = process_departures_response(data)
-            return processed
-        except HTTPException as e:
-            raise e
-        except Exception as e:
-            raise HTTPException(status_code=500, detail=str(e))
+    try:
+        data = await rtt_client.get_departures(
+            origin_station_code, destination_station_code
+        )
+        processed = process_departures_response(data)
+        return processed
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
