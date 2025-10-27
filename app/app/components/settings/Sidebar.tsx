7a84a620bdc062fbd058a3051fd13328a1336d89
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2025-10-27 17:23:17 +0000

    run formatter

--- app/app/components/settings/Sidebar.tsx
+++ app/app/components/settings/Sidebar.tsx
@@ -21,28 +21,28 @@ export default function Sidebar({
   selectedId,
   setSelectedId,
 }: SidebarProps) {
-    return (
-      <aside>
-        <h3 className="font-semibold mb-4">Stations</h3>
-        <input
-          type="text"
-          placeholder="Search stations..."
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          className="mb-4 px-2 py-1 border w-full rounded"
-        />
-        <div className="h-[500px] overflow-y-auto">
-          <ul>
-            {items.map((item, idx) => (
-              <SidebarListItem
-                key={idx}
-                item={item}
-                selected={selectedId === item.naptanID}
-                onClick={setSelectedId}
-              />
-            ))}
-          </ul>
-        </div>
-      </aside>
-    );
+  return (
+    <aside>
+      <h3 className="font-semibold mb-4">Stations</h3>
+      <input
+        type="text"
+        placeholder="Search stations..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="mb-4 px-2 py-1 border w-full rounded"
+      />
+      <div className="h-[500px] overflow-y-auto">
+        <ul>
+          {items.map((item, idx) => (
+            <SidebarListItem
+              key={idx}
+              item={item}
+              selected={selectedId === item.naptanID}
+              onClick={setSelectedId}
+            />
+          ))}
+        </ul>
+      </div>
+    </aside>
+  );
 }
