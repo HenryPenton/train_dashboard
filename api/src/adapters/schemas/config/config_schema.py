706c7bfb20bacebad5c38cb1b93005a45625f527
Henry Penton <henry.penton@gmail.com>
2025-10-27 05:53:35 +0000

    move to using a schema for config

---
+++ api/src/adapters/schemas/config/config_schema.py
@@ -0,0 +1,38 @@
+from marshmallow import EXCLUDE, Schema, fields, post_load
+
+
+class TubeRouteSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    origin = fields.Str(required=True)
+    originNaPTANOrATCO = fields.Str(required=True)
+    destination = fields.Str(required=True)
+    destinationNaPTANOrATCO = fields.Str(required=True)
+
+
+class RailDepartureSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    origin = fields.Str(required=True)
+    originCode = fields.Str(required=True)
+    destination = fields.Str(required=True)
+    destinationCode = fields.Str(required=True)
+
+
+class ConfigSchema(Schema):
+    class Meta:
+        unknown = EXCLUDE
+
+    tfl_best_routes = fields.List(fields.Nested(TubeRouteSchema), load_default=[])
+    rail_departures = fields.List(fields.Nested(RailDepartureSchema), load_default=[])
+    show_tfl_lines = fields.Bool(load_default=False)
+
+    @post_load
+    def set_defaults(self, data, **kwargs):
+        # Ensure lists are present even if missing
+        data.setdefault("tfl_best_routes", [])
+        data.setdefault("rail_departures", [])
+        data["show_tfl_lines"] = bool(data.get("show_tfl_lines", False))
+        return data
