919f6f79a0b9b13962e2b763d8553f0a6a2004a6
Henry Penton <henry.penton@gmail.com>
2025-10-28 11:56:45 +0000

    move config to a pydantic model

--- api/src/application/config_service.py
+++ api/src/application/config_service.py
@@ -2,26 +2,22 @@ from pathlib import Path
 
 from src.adapters.file_handlers.json.json_file_read import JSONFileReader
 from src.adapters.file_handlers.json.json_file_write import JSONFileWriter
-from src.schemas.bidirectional.config.config_schema import ConfigSchema
+from src.schemas.bidirectional.config.config_model import ConfigModel
 
 CONFIG_PATH = Path(__file__).parent.parent.parent / "config/config.json"
 
 
 class ConfigService:
+
     @staticmethod
     def set_config(new_config: dict):
-        config_schema = ConfigSchema()
-        config = config_schema.load(new_config)
-
+        config = ConfigModel(**new_config)
         writer = JSONFileWriter(CONFIG_PATH)
-        writer.write_json(config_schema.dump(config))
+        writer.write_json(config.dict())
         return True
 
     @staticmethod
     def get_config():
         adapter = JSONFileReader(CONFIG_PATH)
-
-        config_schema = ConfigSchema()
-        config = config_schema.load(adapter.read_json())
-
-        return config_schema.dump(config)
+        config = ConfigModel(**adapter.read_json())
+        return config.dict()
