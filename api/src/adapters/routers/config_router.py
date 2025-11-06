8536d746d031d926034b7b3a858af29d72198a96
Henry Penton <henry.penton@gmail.com>
2025-11-06 16:36:28 +0000

    add logging to the config service

--- api/src/adapters/routers/config_router.py
+++ api/src/adapters/routers/config_router.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from fastapi import APIRouter, Depends, HTTPException
@@ -11,13 +12,34 @@ from src.DTOs.config.config_dto import ConfigDTO
 router = APIRouter()
 
 
+def configure_logger():
+    logger = logging.getLogger("config_logger")
+
+    if not logger.hasHandlers():
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            "[%(asctime)s] %(levelname)s %(name)s: %(message)s"
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    return logger
+
+
+def get_logger():
+    return logging.getLogger("config_logger")
+
+
 def get_config_service():
     CONFIG_PATH = Path(__file__).parents[3] / "config/config.json"
+    logger = configure_logger()
 
+    logger.info("Creating ConfigService")
     return ConfigService(
         reader=JSONFileReader[ConfigDAO](CONFIG_PATH),
         writer=JSONFileWriter(CONFIG_PATH),
         config_path=CONFIG_PATH,
+        logger=logger,
     )
 
 
@@ -26,10 +48,13 @@ async def set_config(
     request: ConfigDTO,
     config_service: ConfigService = Depends(get_config_service),
 ):
+    logger = get_logger()
     try:
         config_service.set_config(request)
+        logger.info("Setting config")
         return JSONResponse(content={"status": "ok"})
     except Exception as e:
+        logger.error("Error setting config")
         raise HTTPException(status_code=500, detail=str(e))
 
 
@@ -37,10 +62,14 @@ async def set_config(
 def get_config(
     config_service: ConfigService = Depends(get_config_service),
 ):
+    logger = get_logger()
     try:
         config_data = config_service.get_config()
+        logger.info("Getting config")
         return config_data
     except FileNotFoundError:
+        logger.error("Config file not found")
         raise HTTPException(status_code=404, detail="Config file not found")
     except Exception as e:
+        logger.error("Error getting config")
         raise HTTPException(status_code=500, detail=str(e))
--- api/src/application/config_service.py
+++ api/src/application/config_service.py
@@ -8,19 +8,30 @@ from src.DTOs.config.config_dto import ConfigDTO
 
 class ConfigService:
     def __init__(
-        self, reader: AbstractFileReader, writer: AbstractFileWriter, config_path: Path
+        self,
+        reader: AbstractFileReader,
+        writer: AbstractFileWriter,
+        config_path: Path,
+        logger,
     ):
         self.reader = reader
         self.writer = writer
         self.config_path = config_path
+        self.logger = logger
 
     def set_config(self, new_config: ConfigDTO):
         config = ConfigDAO(**new_config.model_dump())
+        self.logger.info("Setting new config: %s", config)
         self.writer.write_json(config.model_dump())
         return True
 
     def get_config(self):
         if not self.config_path.exists():
+            self.logger.info(
+                "Config file not found, creating default config at %s",
+                self.config_path,
+            )
             self.writer.write_json(ConfigDAO().model_dump())
         config = ConfigDTO(**self.reader.read_json())
+        self.logger.debug("Loaded config: %s", config)
         return config
