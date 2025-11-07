346dc0c874ae4e46d64ab296a0f36a24232da69b
Henry Penton <henrypenton@Henrys-MacBook-Air.local>
2025-11-07 11:06:07 +0000

    add custom ntfy server

--- push/src/ntfy/ntfy.py
+++ push/src/ntfy/ntfy.py
@@ -1,6 +1,13 @@
+import os
 from python_ntfy import NtfyClient
 
 
 def send_ntfy_notification(topic, message):
-    client = NtfyClient(topic=topic)
+    server = os.environ.get("NTFY_SERVER", None)
+    client = None
+    if server is not None:
+        client = NtfyClient(topic=topic, server=server)
+    else:
+        client = NtfyClient(topic=topic)
+
     client.send(message, format_as_markdown=True)
